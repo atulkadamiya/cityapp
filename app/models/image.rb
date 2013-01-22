618627cd11e8dae3581b938b4a0cc723b8d38df8
abhishekjain16 <abhishek.jain@vinsol.com>
2013-01-23 00:22:19 +0530

    config change

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,8 +6,6 @@ class Image < ActiveRecord::Base
   :styles => {
     :thumb=> "100x100#",
   },
-  :storage => :s3,
-  :s3_credentials => "#{Rails.root}/config/s3.yml",
   :path => "/:style/:id/:filename"
 
 
