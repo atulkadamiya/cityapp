a80148b6e9339163213b53df0176a7b338090633
abhishekjain16 <abhishek.jain@vinsol.com>
2013-02-25 16:17:26 +0530

    master

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -30,12 +30,13 @@ class SharesController < ApplicationController
   end
 
   def create
-    @share = Share.new(params[:share])    
+    @share = Share.new(params[:share]) if params[:share].present?
+    @share = Share.new(:image => params[:image]) if params[:image].present?
     respond_to do |format|
       if @share.save
         # format.html { redirect_to @share, notice: 'Shared Image was successfully created.' }
-        format.html { render json: share_url(@share), :status => 200 }
-        format.json { render json: share_url(@share) }        
+        format.html { render json: @share.image.url(:original), :status => 200 }
+        format.json { render json: @share.image.url(:original) }        
       else        
         format.html { render action: "new" }
       end
