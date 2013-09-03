952317f5c9093e15313cc04aa2ab2861a1169296
Abhishek <abhishek.jain@vinsol.com>
2013-09-03 20:49:36 +0530

    fix syntax error

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -31,9 +31,14 @@ class PurchasesController < ApplicationController
 
   def index
     @subscription = Subscription.where(:app_id => params[:app_id], :user_id => params[:user_id]).last
-    @issues = Issue.where('issue_publish_date >= ? and issue_publish_date <= ?', @subscription.effective_date, @subscription.expiration_date)
-  	purchased_issues = @issues.collect(&:product_identifier)
-  	render :json => { :purchased_issues => purchased_issues, :status => "success", :subscription_active => (subscription.present? && subscription.expiration_date > DateTime.current) }
+    if @subscription
+      @issues = Issue.where('issue_publish_date >= ? and issue_publish_date <= ?', @subscription.effective_date, @subscription.expiration_date)
+      purchased_issues = @issues.collect(&:product_identifier)
+      render :json => { :purchased_issues => purchased_issues, :status => "success", :subscription_active => (@subscription.present? && @subscription.expiration_date > DateTime.current) }
+    else
+      purchased_issues = []
+      render :json => { :purchased_issues => purchased_issues, :status => "success", :subscription_active => (@subscription.present? && @subscription.expiration_date > DateTime.current) }      
+    end
   end
 
   private
